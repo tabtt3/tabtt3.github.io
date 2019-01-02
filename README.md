3c71acbf2e8f46ed04ec3fd3a56e7da709d944cf
tabtt3 <tabttppp@gmail.com>
2019-01-02 21:37:40 +0900

    modify

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-# STATIC
+# tabtt3.github.io
 
-This directory contains your static files.
-Each file inside this directory is mapped to /.
+> My good Nuxt.js project
 
-Example: /static/robots.txt is mapped as /robots.txt.
+## Build Setup
 
-More information about the usage of this directory in the documentation:
-https://nuxtjs.org/guide/assets#static
+``` bash
+# install dependencies
+$ yarn install
 
-**This directory is not required, you can delete it if you don't want to use it.**
+# serve with hot reload at localhost:3000
+$ yarn run dev
 
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="top-content">
+    <section class="container">
+      <nav>
+        <ul>
+          <li>
+            <nuxt-link to="/">
+              <div class="top-menu-item">
+                <span class="top-menu-item-main">HOME</span>
+                <span class="top-menu-item-supplement">ホーム</span>
+              </div>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">
+              <div class="top-menu-item">
+                <span class="top-menu-item-main">Company</span>
+                <span class="top-menu-item-supplement">会社概要</span>
+              </div>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">
+              <div class="top-menu-item">
+                <span class="top-menu-item-main">Service</span>
+                <span class="top-menu-item-supplement">サービス案内</span>
+              </div>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">
+              <div class="top-menu-item">
+                <span class="top-menu-item-main">Recuit</span>
+                <span class="top-menu-item-supplement">採用情報</span>
+              </div>
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">
+              <div class="top-menu-item">
+                <span class="top-menu-item-main">Contact</span>
+                <span class="top-menu-item-supplement">問い合わせ</span>
+              </div>
+            </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+}
+</script>
+
+<style>
+.top-content {
+	min-height: 100%;
+  background: url('~assets/images/bg.png') no-repeat right top/40%;
+	overflow: auto;
+}
+
+.top-menu-item {
+  width: 500px;
+  font-size:12px;
+  line-height: 1;
+  text-align: center;
+  padding-top: 60px;
+}
+
+.top-menu-item-main {
+  font-size: 40px;
+  display: block;
+  padding-bottom: 10px;
+	font-family: 'Julius Sans One', sans-serif;	/*Google Fontsの指定*/
+}
+.top-menu-item-supplement {
+}
+</style>
+
