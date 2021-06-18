13ee126f07b0dd7274611df21437844e4e7110f5
tabtt3 <tabttppp@gmail.com>
2021-06-19 03:40:51 +0900

    vueでやりなおし

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div>
+      <img alt="profile-img" src="./assets/mike.jpg" width="200px" class="profile-img">
+      <h1>tabtt</h1>
+      <h2></h2>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -24,5 +26,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.profile-img {
+  border-radius: 10px;
 }
 </style>
